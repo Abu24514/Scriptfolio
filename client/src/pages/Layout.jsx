0635f47390363a8c3f5d1d279fb8eda0403e969b
Abu Hanjala <abuhanjala656@gmail.com>
2026-05-03 18:19:35 +0530

    create nav of dashboard

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Nav from '../components/Nav/Nav'
 
 const Layout = () => {
   return (
     <div>
-        <h1>Layout</h1>
-        <div><Outlet/></div>
+        <div className='min-h-screen bg-gray-50'>
+          <Nav/>
+          <Outlet/>
+          </div>
     </div>
   )
 }
