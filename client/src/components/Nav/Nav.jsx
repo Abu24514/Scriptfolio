4a0032a0fcd8bc1b5e43394db6580cd365fd6287
Abu Hanjala <abuhanjala656@gmail.com>
2026-05-04 17:47:21 +0530

    fix nav logo and create resume builder UI

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-
+import logo from '../../assets/scriptfolio-logo.svg'
 const Nav = () => {
     const user = { name: " Alex John" }
     const navigate = useNavigate();
@@ -11,7 +11,7 @@ const Nav = () => {
         <div className='shadow bg-white'>
             <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5'>
                  <Link to="/" className="flex items-center gap-2">
-                <img className="h-18 w-auto" src="src/assets/scriptfolio-logo.svg" alt="logo" />
+                <img className="h-18 w-auto" src={logo} alt="logo" />
                 <span className="text-lg sm:text-xl font-semibold hover:text-indigo-600">
                     Scriptfolio
                 </span>
