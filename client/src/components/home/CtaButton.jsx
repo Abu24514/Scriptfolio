9b27ffb52dad25bd9ad0fb5192ded4ada5820e35
Abu Hanjala <abuhanjala656@gmail.com>
2026-05-24 17:32:36 +0530

    fix the CtaButton

--- client/src/components/home/CtaButton.jsx
+++ client/src/components/home/CtaButton.jsx
@@ -1,9 +1,9 @@
 import { MoveRight } from "lucide-react";
-
+import { Link } from "react-router-dom";
 const CtaButton = () => {
   return (
     
-     <a href="#"
+     <Link to ='/app'
       className=" mt-5 sm:mt-8
       inline-flex items-center bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-800 text-slate-100 pl-2 pr-6 sm:pr-8 py-2 rounded-full text-sm sm:text-base font-medium transition-colors group capitalize"
     >
@@ -13,7 +13,7 @@ const CtaButton = () => {
       </div>
 
       Build my resume
-    </a>
+    </Link>
   );
 };
 
